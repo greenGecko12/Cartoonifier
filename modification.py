f6d2cb24d07792052fe3d9fb05260f46b6b00572
Sai Kommuru <sai.kommuru123@gmail.com>
2023-02-13 15:20:49 +0000

    removed html files

--- modification.py
+++ modification.py
@@ -1,3 +1,5 @@
 """
 This file will allow the user to modify the generated image by performing some basic edits: skin colour, hair colour etc.
+
+Use the facial_editing directory to do the work.
 """
